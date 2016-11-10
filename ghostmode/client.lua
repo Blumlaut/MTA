fbbb62258f9cad60755bffb10e86228ac33b4849
Blue <ericmager@gmx.de>
2016-11-10 23:13:34 +0100

    introducing ghost mode!
    Sick of rammers? Watch how he made himself seetrough in one simple
    trick, physics hate him!

---
+++ ghostmode/client.lua
@@ -0,0 +1,50 @@
+ghostVehicles = {}
+
+
+addEvent("toggleGhostMode",true)
+
+function toggleGhostMode(obj,mode)
+	if mode == true then
+		for theKey,theVehicle in ipairs(getElementsByType("vehicle")) do
+			setElementCollidableWith(obj, theVehicle,false)
+			setElementAlpha(obj, 200)
+		end
+		table.insert ( ghostVehicles, obj )
+		
+	end
+	if mode == false then
+		for theKey,theVehicle in ipairs(getElementsByType("vehicle")) do
+		outputDebugString("br")
+			setElementCollidableWith(obj, theVehicle,true)
+			setElementAlpha(obj, 255)
+		end
+		
+		
+		
+		for index, vehicle in ipairs ( ghostVehicles ) do
+			if ( obj == vehicle ) then
+				table.remove ( ghostVehicles, index )
+			end
+		end
+		
+		
+		
+	end
+end
+addEventHandler("toggleGhostMode",root, toggleGhostMode,true)
+
+
+
+
+function ghostmodeCheck()
+	for theKey,theVehicle in ipairs(getElementsByType("vehicle")) do
+		if isElementStreamedIn(theVehicle) then
+			for theKey,theGhost in ipairs(ghostVehicles) do
+				if isElementStreamedIn(theGhost) then
+					setElementCollidableWith(theGhost,theVehicle,false)
+				end
+			end
+		end
+	end
+end
+setTimer(ghostmodeCheck,3000,0)
---
+++ ghostmode/server.lua
@@ -0,0 +1,49 @@
+ghostTable = {}
+
+function GhostModeCMD(source)
+if getElementData(source, "hasMostWanted") then return end
+	if getPedOccupiedVehicle(source) and source == getVehicleOccupant(getPedOccupiedVehicle(source), 0) then
+		if getElementData(getPedOccupiedVehicle(source), "ghostmode") then
+			toggleGhostModeS(getPedOccupiedVehicle(source),false)
+			outputChatBox("Ghost Mode turned OFF",source, 120,120,0)
+			setElementData(getPedOccupiedVehicle(source), "ghostmode", false)
+	
+		for index, vehicle in ipairs ( ghostTable ) do
+			if ( getPedOccupiedVehicle(source) == vehicle ) then
+				table.remove ( ghostTable, index )
+			end
+		end
+
+		elseif getElementData(getPedOccupiedVehicle(source), "ghostmode") == false then
+			toggleGhostModeS(getPedOccupiedVehicle(source),true)
+			outputChatBox("Ghost Mode turned ON",source, 120,120,0)
+			table.insert ( ghostTable, getPedOccupiedVehicle(source) )
+			setElementData(getPedOccupiedVehicle(source), "ghostmode", true)
+		end
+	end
+end
+addCommandHandler("ghost",GhostModeCMD,false,false)
+
+
+function toggleGhostModeS(obj,mode)
+
+triggerClientEvent(root, "toggleGhostMode",resourceRoot, obj,mode)
+
+
+end
+
+
+function elementDestroyHandler()
+if getElementType(source) == "vehicle" then
+toggleGhostModeS(source,false)
+end
+end
+addEventHandler("onElementDestroy", root, elementDestroyHandler)
+
+
+function vehicleExitHandler(veh,seat)
+	if seat == 0 and getElementData(veh, "ghostmode") then
+		toggleGhostModeS(veh,false)
+	end
+end
+addEventHandler("onPlayerVehicleExit", root, vehicleExitHandler)
